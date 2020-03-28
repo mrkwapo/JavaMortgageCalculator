3c68be3f352127c6a574bb800bbb1d6aabb30d81
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-28 17:18:29 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.Dracodess;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterestRate = (float) Console.readNumber("Annual Interest Rate: ", 1,30);
+        byte years = (byte) Console.readNumber("Period (Years): ",1,30);
+
+        var calculator = new MortgageCalculator( principal, annualInterestRate, years);
+        var report = new MortgageReport(calculator);
+        report.printMortgage();
+        report.printPaymentSchedule();
+    }
+
+}
